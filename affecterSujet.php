172d55ef678916571b1df6baaec4897bbec765f9
fatima <larbifatima94@gmail.com>
2018-03-26 19:50:32 +0200

    donner la main à l'administration pour gerer le nombre de sujet proposée par chaque enseignant

---
+++ affecterSujet.php
@@ -0,0 +1,157 @@
+<?php
+include("cnxbdd.php");
+if(isset($_POST['submit'])) {
+
+
+
+        $sql = $db->prepare('insert into nbrsujet (nbr) 
+ values (?)');
+        $params = array($_POST['nbr']);
+        $sql->execute($params);
+
+
+}
+?>
+<!DOCTYPE html>
+<body lang="en">
+<head>
+    <title>PFE MANAGER</title>
+    <!-- for-mobile-apps -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Controller Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //for-mobile-apps -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- js -->
+    <style>
+        #id{
+            background: url(images/hy.jpg) no-repeat 0px 0px;
+
+            background-size: cover;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            -ms-background-size: cover;
+            min-height: 350px;
+        }
+        .border {
+            border: 1px solid #009;
+            -webkit-border-radius: 10px;
+            -moz-border-radius: 10px;
+            border-radius: 10px;
+            -webkit-box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.75);
+            -moz-box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.75);
+            box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.75);
+            Background-Color: #BCC4C3
+        ;
+
+
+        }
+        label {
+            display: block;
+            margin-left: 3px;
+            padding-top: 2px;
+            text-shadow: 2px 2px 3px rgba(150, 150, 150, 0.75);
+            font-family:Verdana, Geneva, sans-serif;
+            font-size:.9em;
+        }
+
+        legend {
+            color:red;
+            text-shadow: 2px 2px 3px rgba(150, 150, 150, 0.75);
+            font-family:Verdana, Geneva, sans-serif;
+            font-size:1.4em;
+            border-top: 2px solid #009;
+            border-left: 2px solid #009;
+            border-right:  2px solid #009;
+            border-radius: 10px;
+            -webkit-box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.75);
+            -moz-box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.75);
+            box-shadow: 4px 4px 5px rgba(50, 50, 50, 0.75);
+            padding: 3px;
+        }
+
+
+    </style>
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <!-- //js -->
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <!-- start-smoth-scrolling -->
+
+    <script type="text/javascript" src="js/move-top.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+            });
+        });
+    </script>
+    <!-- start-smoth-scrolling -->
+</head>
+
+<div>>
+    <!-- banner -->
+    <div id="id" class="banner two" >
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="navbar-header navbar-left">
+                    <img height='100 px' width='200 px' src='./images/151871840444015710.png' />
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <ul class="top-links">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                </ul>
+                <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+
+                        <li class="active" ><a href="#">Proposer Sujet</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="portfolio.html">Deconnexion</a></li>
+
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </nav>
+            <script>
+                $('ul.nav li.dropdown').hover(function() {
+                    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+                }, function() {
+                    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+                });
+            </script>
+        </div>
+        <div class="clearfix"></div>
+
+        <div class="w3l-sub-head">
+        </div>
+    </div><br>
+    <body>
+    <form method="post" action="" enctype="multipart/form-data"  >
+    <h3>Entrez le nbr de sujet proposée </h3><br>
+    <input type="number" name="nbr" /><br><br>
+   <input  type='submit' name='submit' class='btn btn-danger' value='Enregistrer'  ></div>
+</form>
+
+
+
+
+
+
+    </body>
